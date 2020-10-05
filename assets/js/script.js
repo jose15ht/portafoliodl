c955bff58bdc5309a523065f248b25e505033639
Jose Herrera <joseignacioht@gmail.com>
2020-10-05 13:12:41 -0300

    finished all sections,defined css and js files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+$(function (){
+
+  $("a").click(function(event){
+    // alert(this.hash)
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var gaco = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(gaco).offset().top
+      }, 750, function(){
+
+        window.location.hash = gaco;
+      })
+    }
+  });
+})
+
 $(function () {
-   $('[data-toggle="tooltip"]').tooltip()
- })
+  $('[data-toggle="tooltip"]').tooltip()
+})
